17eb8e512b4209b18b55ab21e802867c56ad04c0
LearnProcessing <rinaldiadrian5@gmail.com>
2021-03-19 21:57:32 +0700

    participant controller and router added

--- routes/RecipientRouter.js
+++ routes/RecipientRouter.js
@@ -1,4 +1,10 @@
 const RecipientRouter = require("express").Router();
 const RecipientContoller = require("../controller/RecipientController");
 
+RecipientRouter.post("/recipients/:eventId", RecipientContoller.addRecipients)
+RecipientRouter.get("/recipients/:certificateNumber", RecipientContoller.getRecipient)
+RecipientRouter.delete("/recipients/:recipientId", RecipientContoller.deleteRecipient)
+RecipientRouter.put("/recipients/:eventId/:recipientId", RecipientContoller.putRecipient)
+
+
 module.exports = RecipientRouter;
