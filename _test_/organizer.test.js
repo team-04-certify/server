20fb8d751d6b175003d2cb3d1ef1bbd7e05ca462
neuthos <ardiangalang05@gmail.com>
2021-03-20 08:46:53 +0700

    fix event-test error and dialect wrong

--- _test_/organizer.test.js
+++ _test_/organizer.test.js
@@ -1,26 +1,28 @@
-const request = require('supertest')
-const app = require('../app')
-const {Organizer, Event, Recipient} = require("../models")
-const jwt = require('jsonwebtoken')
+const request = require("supertest");
+const app = require("../app");
+const { Organizer, Event, Recipient } = require("../models");
+const jwt = require("jsonwebtoken");
 
-let access_token = ''
-let organizerName = ''
+let access_token = "";
+let organizerName = "";
 beforeEach(() => {
   const organizerData = {
-    name: 'Certify',
-    email: 'admin@mail.com',
-    password: '123456'
-  }
-  access_token =  jwt.sign({name: organizerData.name, email: organizerData.email}, 'certifyjayaaaselole123123');
+    name: "Certify",
+    email: "admin@mail.com",
+    password: "123456",
+  };
+  access_token = jwt.sign(
+    { name: organizerData.name, email: organizerData.email },
+    "certifyjayaaaselole123123"
+  );
   Organizer.create(organizerData)
-    .then(response => {
-      organizerName = response.name
+    .then((response) => {
+      organizerName = response.name;
     })
-    .catch(err => {
-        console.log(err)
-    })
-
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
 afterAll(() => {
   Organizer.destroy({ truncate: true })
     .then((response) => {})
@@ -33,252 +35,250 @@ afterAll(() => {
   Recipient.destroy({ truncate: true })
     .then((response) => {})
     .catch((response) => {});
-}); 
+});
 
 // ============ register success============
-describe('POST /register', function() {
-  it('should return status 201', function(done) {
+describe("POST /register", function () {
+  it("should return status 201", function (done) {
     let body = {
-      name: 'Certify Seminar',
-      email: 'robi@mail.com',
-      password: 'robi123'
-    }
+      name: "Certify Seminar",
+      email: "robi@mail.com",
+      password: "robi123",
+    };
     request(app)
-      .post('/register')
+      .post("/register")
       .send(body)
       .end((err, res) => {
-        if(err) {
-          done(err)
+        if (err) {
+          done(err);
         }
-        expect(res.status).toEqual(201)
-        expect(typeof(res.body)).toEqual('object')
-        expect(res.body).toHaveProperty('name')
-        expect(typeof(res.body.name)).toEqual('string')
-        expect(res.body).toHaveProperty('email')
-        expect(typeof(res.body.email)).toEqual('string')
-        done()
-      })
-  })
-})
+        expect(res.status).toEqual(201);
+        expect(typeof res.body).toEqual("object");
+        expect(res.body).toHaveProperty("name");
+        expect(typeof res.body.name).toEqual("string");
+        expect(res.body).toHaveProperty("email");
+        expect(typeof res.body.email).toEqual("string");
+        done();
+      });
+  });
+});
 // ============ register error name unique ============
-describe('POST /register', function() {
-  it('should return status 201', function(done) {
+describe("POST /register", function () {
+  it("should return status 201", function (done) {
     let body = {
-      name: 'Certify Seminar',
-      email: 'robi@mail.com',
-      password: 'robi123'
-    }
+      name: "Certify Seminar",
+      email: "robi@mail.com",
+      password: "robi123",
+    };
     request(app)
-      .post('/register')
+      .post("/register")
       .send(body)
       .end((err, res) => {
-        if(err) {
-          done(err)
+        if (err) {
+          done(err);
         }
-        expect(res.status).toEqual(400)
-        expect(Array.isArray(res.body)).toEqual(true)
-        expect((res.body[0])).toHaveProperty('message')
-        done()
-      })
-  })
-})
+        expect(res.status).toEqual(400);
+        expect(Array.isArray(res.body)).toEqual(true);
+        expect(res.body[0]).toHaveProperty("message");
+        done();
+      });
+  });
+});
 
 // ============ register error email unique ============
-describe('POST /register', function() {
-  it('should return status 201', function(done) {
+describe("POST /register", function () {
+  it("should return status 201", function (done) {
     let body = {
-      name: 'Certify Seminar2',
-      email: 'robi@mail.com',
-      password: 'robi123'
-    }
+      name: "Certify Seminar2",
+      email: "robi@mail.com",
+      password: "robi123",
+    };
     request(app)
-      .post('/register')
+      .post("/register")
       .send(body)
       .end((err, res) => {
-        if(err) {
-          done(err)
+        if (err) {
+          done(err);
         }
-        expect(res.status).toEqual(400)
-        expect(Array.isArray(res.body)).toEqual(true)
-        expect((res.body[0])).toHaveProperty('message')
-        done()
-      })
-  })
-})
+        expect(res.status).toEqual(400);
+        expect(Array.isArray(res.body)).toEqual(true);
+        expect(res.body[0]).toHaveProperty("message");
+        done();
+      });
+  });
+});
 
 // ============ register error validation password min length 6 characters============
-describe('POST /register', function() {
-  it('should return status 201', function(done) {
+describe("POST /register", function () {
+  it("should return status 201", function (done) {
     let body = {
-      name: 'Seminar',
-      email: 'kantorSeminar@gmail.com',
-      password: '23'
-    }
+      name: "Seminar",
+      email: "kantorSeminar@gmail.com",
+      password: "23",
+    };
     request(app)
-      .post('/register')
+      .post("/register")
       .send(body)
       .end((err, res) => {
-        if(err) {
-          done(err)
+        if (err) {
+          done(err);
         }
-        expect(res.status).toEqual(400)
-        expect(Array.isArray(res.body)).toEqual(true)
-        expect((res.body[0])).toHaveProperty('message')
-        done()
-      })
-  })
-})
+        expect(res.status).toEqual(400);
+        expect(Array.isArray(res.body)).toEqual(true);
+        expect(res.body[0]).toHaveProperty("message");
+        done();
+      });
+  });
+});
 
 // ============ register error validation password min length 6 characters and empty name and empty email============
-describe('POST /register', function() {
-  it('should return status 201', function(done) {
+describe("POST /register", function () {
+  it("should return status 201", function (done) {
     let body = {
-      name: '',
-      email: '',
-      password: '23'
-    }
+      name: "",
+      email: "",
+      password: "23",
+    };
     request(app)
-      .post('/register')
+      .post("/register")
       .send(body)
       .end((err, res) => {
-        if(err) {
-          done(err)
+        if (err) {
+          done(err);
         }
-        expect(res.status).toEqual(400)
-        expect(Array.isArray(res.body)).toEqual(true)
-        done()
-      })
-  })
-})
+        expect(res.status).toEqual(400);
+        expect(Array.isArray(res.body)).toEqual(true);
+        done();
+      });
+  });
+});
 // ============ login success============
-describe('POST /login', function() {
-  it('should return status 200', function(done) {
+describe("POST /login", function () {
+  it("should return status 200", function (done) {
     let body = {
-      email: 'robi@mail.com',
-      password: 'robi123'
-    }
+      email: "robi@mail.com",
+      password: "robi123",
+    };
     request(app)
-      .post('/login')
+      .post("/login")
       .send(body)
       .end((err, res) => {
-        if(err) {
-          done(err)
+        if (err) {
+          done(err);
         }
-        expect(res.status).toEqual(200)
-        expect(typeof(res.body)).toEqual('object')
-        expect(res.body).toHaveProperty('access_token')
-        done()
-      })
-  })
-})
+        expect(res.status).toEqual(200);
+        expect(typeof res.body).toEqual("object");
+        expect(res.body).toHaveProperty("access_token");
+        done();
+      });
+  });
+});
 // ============ login error  no email adn password=============
-describe('POST /login', function() {
-  it('should return status 400', function(done) {
+describe("POST /login", function () {
+  it("should return status 400", function (done) {
     let body = {
-      email: '',
-      password: ''
-    }
+      email: "",
+      password: "",
+    };
     request(app)
-      .post('/login')
+      .post("/login")
       .send(body)
       .end((err, res) => {
-        if(err) {
-          done(err)
+        if (err) {
+          done(err);
         }
-        expect(res.status).toEqual(400)
-        expect(typeof(res.body)).toEqual('object')
-        expect(res.body).toHaveProperty('message')
-        expect(res.body.message).toEqual('invalid email or password')
-        done()
-      })
-  })
-})
+        expect(res.status).toEqual(400);
+        expect(typeof res.body).toEqual("object");
+        expect(res.body).toHaveProperty("message");
+        expect(res.body.message).toEqual("invalid email or password");
+        done();
+      });
+  });
+});
 // ============ login error email is empty in database=============
-describe('POST /login', function() {
-  it('should return status 400', function(done) {
+describe("POST /login", function () {
+  it("should return status 400", function (done) {
     let body = {
-      email: 'tahu@mail.com',
-      password: 'robi123'
-    }
+      email: "tahu@mail.com",
+      password: "robi123",
+    };
     request(app)
-      .post('/login')
+      .post("/login")
       .send(body)
       .end((err, res) => {
-        if(err) {
-          done(err)
+        if (err) {
+          done(err);
         }
-        expect(res.status).toEqual(400)
-        expect(typeof(res.body)).toEqual('object')
-        expect(res.body).toHaveProperty('message')
-        expect(res.body.message).toEqual('invalid email or password')
-        done()
-      })
-  })
-})
+        expect(res.status).toEqual(400);
+        expect(typeof res.body).toEqual("object");
+        expect(res.body).toHaveProperty("message");
+        expect(res.body.message).toEqual("invalid email or password");
+        done();
+      });
+  });
+});
 // ============ login error  no email=============
-describe('POST /login', function() {
-  it('should return status 400', function(done) {
+describe("POST /login", function () {
+  it("should return status 400", function (done) {
     let body = {
-      email: '',
-      password: 'robi123'
-    }
+      email: "",
+      password: "robi123",
+    };
     request(app)
-      .post('/login')
+      .post("/login")
       .send(body)
       .end((err, res) => {
-        if(err) {
-          done(err)
+        if (err) {
+          done(err);
         }
-        expect(res.status).toEqual(400)
-        expect(typeof(res.body)).toEqual('object')
-        expect(res.body).toHaveProperty('message')
-        expect(res.body.message).toEqual('invalid email or password')
-        done()
-      })
-  })
-})
+        expect(res.status).toEqual(400);
+        expect(typeof res.body).toEqual("object");
+        expect(res.body).toHaveProperty("message");
+        expect(res.body.message).toEqual("invalid email or password");
+        done();
+      });
+  });
+});
 // ============ login error  no password=============
-describe('POST /login', function() {
-  it('should return status 400', function(done) {
+describe("POST /login", function () {
+  it("should return status 400", function (done) {
     let body = {
-      email: 'robi@mail.com',
-      password: ''
-    }
+      email: "robi@mail.com",
+      password: "",
+    };
     request(app)
-      .post('/login')
+      .post("/login")
       .send(body)
       .end((err, res) => {
-        if(err) {
-          done(err)
+        if (err) {
+          done(err);
         }
-        expect(res.status).toEqual(400)
-        expect(typeof(res.body)).toEqual('object')
-        expect(res.body).toHaveProperty('message')
-        expect(res.body.message).toEqual('invalid email or password')
-        done()
-      })
-  })
-})
-
+        expect(res.status).toEqual(400);
+        expect(typeof res.body).toEqual("object");
+        expect(res.body).toHaveProperty("message");
+        expect(res.body.message).toEqual("invalid email or password");
+        done();
+      });
+  });
+});
 
 // ================== show organization by id==================
-describe('GET /:organizername', function() {
-  it('should return status 200', function(done) {
+describe("GET /:organizername", function () {
+  it("should return status 200", function (done) {
     request(app)
       .get(`/${organizerName}`)
-      .set('access_token', access_token)
+      .set("access_token", access_token)
       .end((err, res) => {
-        if(err) {
-          done(err)
+        if (err) {
+          done(err);
         }
-        expect(res.status).toEqual(200)
-        expect(typeof(res.body)).toEqual('object')
-        expect((res.body)).toHaveProperty('id')
-        expect((res.body)).toHaveProperty('name')
-        expect((res.body)).toHaveProperty('email')
-        expect((res.body)).toHaveProperty('Events')
-        done()
-      })
-  })
-})
-
+        expect(res.status).toEqual(200);
+        expect(typeof res.body).toEqual("object");
+        expect(res.body).toHaveProperty("id");
+        expect(res.body).toHaveProperty("name");
+        expect(res.body).toHaveProperty("email");
+        expect(res.body).toHaveProperty("Events");
+        done();
+      });
+  });
+});
--- routes/EventRouter.js
+++ routes/EventRouter.js
@@ -1,4 +1,14 @@
 const EventRouter = require("express").Router();
 const EventController = require("../controller/EventController");
 
+// EventRouter.get("/:organizerName", EventController.getOrganizerEvents);
+
+EventRouter.get("/:organizerName/:eventId", EventController.getOrganizerEvent);
+
+EventRouter.post("/events/:organizerId", EventController.addEvent);
+
+EventRouter.put("/events/:eventId", EventController.editEvent);
+
+EventRouter.delete("/events/:eventId", EventController.deleteEvent);
+
 module.exports = EventRouter;
